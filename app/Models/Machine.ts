37ba9e163bbafba5167023d7cafb83e75c5d7111
balajiofficial <balajiofficialmailme@gmail.com>
2022-11-09 21:35:50 +0530

    machine activities done

--- app/Models/Machine.ts
+++ app/Models/Machine.ts
@@ -5,6 +5,39 @@ export default class Machine extends BaseModel {
   @column({ isPrimary: true })
   public id: number
 
+  @column()
+  public company_id: number
+  @column()
+  public shift_id: number
+  @column()
+  public shift_name: String
+  @column()
+  public machine_id: number
+  @column()
+  public machine_name: String
+  @column()
+  public machine_client_id: String
+  @column()
+  public machine_date: Date
+  @column()
+  public machine_time: DateTime
+
+  @column()
+  public machine_active_status: Boolean
+  @column()
+  public break_type: String
+  @column()
+  public break_reason: String
+  @column()
+  public product_id: number
+  @column()
+  public product_name: String
+  @column()
+  public emp_id: number
+  @column()
+  public emp_name: String
+
+
   @column.dateTime({ autoCreate: true })
   public createdAt: DateTime
 
--- database/migrations/1668002165323_machine_activities.ts
+++ database/migrations/1668002165323_machine_activities.ts
@@ -5,7 +5,30 @@ export default class extends BaseSchema {
 
   public async up () {
     this.schema.createTable(this.tableName, (table) => {
+
       table.increments('id')
+      //company
+      table.integer('company_id')
+      //shift
+      table.integer('shift_id')
+      table.string('shift_name')
+      //machine
+      table.integer('machine_id')
+      table.string('machine_name')
+      table.string('machine_client_id')//main tracking
+      table.date('machine_date')
+      table.time('machine_time')
+      table.string('machine_active_status')
+      table.string('break_type')
+      table.string('break_reason')
+      //product
+      table.integer('product_id')
+      table.string('product_name')
+      //employee
+      table.integer('emp_id')
+      table.string('emp_name')
+
+
 
       /**
        * Uses timestamptz for PostgreSQL and DATETIME2 for MSSQL
